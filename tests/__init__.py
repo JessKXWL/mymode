40e88cf52ad9e137d0a34c06ccfc908191715174
Jess <2482003411@qq.com>
2019-10-25 16:17:57 +0800

    初始化

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+@Author    : Jess
+@Email     : 2482003411@qq.com
+@License   : Copyright(C), Jess
+@Time      : 2019/10/25 16:14
+@File      : __init__.py.py
+@Version   : 1.0
+@Desciption: 
+"""
